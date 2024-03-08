61d1eced83fd6cb92cbc75921fbf2c516bc34648
ahmedibrahim132002@gmail.com <ahmedibrahim132002@gmail.com>
2024-03-08 23:50:40 +0200

    configured the ODU fields data

--- Configuration/OTN_Fields_Config.py
+++ Configuration/OTN_Fields_Config.py
@@ -22,14 +22,14 @@ class OTN_OH(Enum):
 
 # ODU Overheads Config
 
-    ODU_RES1 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=16)
+    ODU_RES1 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=0 , column_end= 1, num_of_bits=16)
 
-    ODU_PM_TCM_DMt1 = OTN_Field_Data(parent_type="ODU_PM_TCM", row_num=0, column_start=2 , num_of_bits=1)
-    ODU_PM_TCM_DMt2 = OTN_Field_Data(parent_type="ODU_PM_TCM", row_num=0, column_start=2, num_of_bits=1)
-    ODU_PM_TCM_DMt3 = OTN_Field_Data(parent_type="ODU_PM_TCM", row_num=0, column_start=2, num_of_bits=1)
-    ODU_PM_TCM_DMt4 = OTN_Field_Data(parent_type="ODU_PM_TCM", row_num = 0, column_start=2, num_of_bits=1)
-    ODU_PM_TCM_DMt5 = OTN_Field_Data(parent_type="ODU_PM_TCM", row_num=0, column_start=2, num_of_bits=1)
-    ODU_PM_TCM_DMt6 = OTN_Field_Data(parent_type="ODU_PM_TCM", row_num=0, column_start=2, num_of_bits=1)
+    ODU_PM_TCM_DMt1 = OTN_Field_Data(parent_type="ODU_PM_TCM" , num_of_bits=1)
+    ODU_PM_TCM_DMt2 = OTN_Field_Data(parent_type="ODU_PM_TCM" ,  num_of_bits=1)
+    ODU_PM_TCM_DMt3 = OTN_Field_Data(parent_type="ODU_PM_TCM" , num_of_bits=1)
+    ODU_PM_TCM_DMt4 = OTN_Field_Data(parent_type="ODU_PM_TCM" , num_of_bits=1)
+    ODU_PM_TCM_DMt5 = OTN_Field_Data(parent_type="ODU_PM_TCM" , num_of_bits=1)
+    ODU_PM_TCM_DMt6 = OTN_Field_Data(parent_type="ODU_PM_TCM" , num_of_bits=1)
 
     ODU_PM_TCM_DMt_inner_levels = {
 
@@ -45,8 +45,8 @@ class OTN_OH(Enum):
     ODU_PM_TCM = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2 , inner_levels=ODU_PM_TCM_DMt_inner_levels)
 
 
-    ODU_EXP1 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2)
-    ODU_EXP2 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2)
+    ODU_EXP1 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=3)
+    ODU_EXP2 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=13)
 
 
     ODU_TCMi_TTI = OTN_Field_Data(parent_type="ODU_TCM")
@@ -64,12 +64,12 @@ class OTN_OH(Enum):
     }
 
 
-    ODU_TCM1 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
-    ODU_TCM2 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
-    ODU_TCM3 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
-    ODU_TCM4 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
-    ODU_TCM5 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
-    ODU_TCM6 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
+    ODU_TCM1 = OTN_Field_Data(parent_type="ODU", row_num=2, column_start=6,column_end=8 , num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
+    ODU_TCM2 = OTN_Field_Data(parent_type="ODU", row_num=2, column_start=3 , column_end = 5 , num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
+    ODU_TCM3 = OTN_Field_Data(parent_type="ODU", row_num=2, column_start=0 , column_end = 2 ,num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
+    ODU_TCM4 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=10, column_end = 12 ,num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
+    ODU_TCM5 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=7, column_end = 9,num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
+    ODU_TCM6 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=4, column_end = 6 ,num_of_bits=24, inner_levels= ODU_TCMi_inner_levels)
 
 
 
@@ -90,17 +90,15 @@ class OTN_OH(Enum):
         'STAT': ODU_PM_STAT,
     }
 
-    ODU_PM = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=24 , inner_levels=ODU_PM_inner_levels)
+    ODU_PM = OTN_Field_Data(parent_type="ODU", row_num=2, column_start=9, column_end=11 ,num_of_bits=24 , inner_levels=ODU_PM_inner_levels)
 
+    ODU_EXP3 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=12 , column_end= 13, num_of_bits=16)
 
+    ODU_GCC1 = OTN_Field_Data(parent_type="ODU", row_num=3, column_start=0, column_end= 1,num_of_bits=16)
+    ODU_GCC2 = OTN_Field_Data(parent_type="ODU", row_num=3, column_start=2, column_end= 3, num_of_bits=16)
 
-    ODU_EXP3 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=16)
-
-    ODU_GCC1 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=16)
-    ODU_GCC2 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=16)
-
-    ODU_APS_PCC = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=32)
-    ODU_RES2 = OTN_Field_Data(parent_type="ODU", row_num=1, column_start=2, num_of_bits=48)
+    ODU_APS_PCC = OTN_Field_Data(parent_type="ODU", row_num=3, column_start=4, column_end=7 ,  num_of_bits=32)
+    ODU_RES2 = OTN_Field_Data(parent_type="ODU", row_num=3, column_start=8, column_end=13 ,num_of_bits=48)
 
 ############################################################################################################
 
