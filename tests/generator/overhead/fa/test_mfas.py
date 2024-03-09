b602f5cd07baed1072478876dd770c1eab44efb8
ammaralmorsi <ammaralmorsi@gmail.com>
2024-03-09 17:09:16 +0200

    refactor: refactor fa generators to use overhead value class as their result.

--- tests/generator/overhead/fa/test_mfas.py
+++ tests/generator/overhead/fa/test_mfas.py
@@ -1,5 +1,6 @@
 import unittest
-from generator.overhead.fa.mfas import MFASOverheadGenerator
+
+from generator.overhead.fa import MFASOverheadGenerator
 
 
 class TestMFASOverheadGenerator(unittest.TestCase):
@@ -7,15 +8,15 @@ class TestMFASOverheadGenerator(unittest.TestCase):
         self.mfas_generator = MFASOverheadGenerator()
 
     def test_starts_from_zero(self):
-        self.assertEqual(self.mfas_generator.get_next_value(), 0)
+        self.assertEqual(self.mfas_generator.get_next_value().as_int, 0)
 
     def test_wrapping_around(self):
         for i in range(256):
-            self.assertEqual(self.mfas_generator.get_next_value(), i)
+            self.assertEqual(self.mfas_generator.get_next_value().as_int, i)
 
-        self.assertEqual(self.mfas_generator.get_next_value(), 0)
+        self.assertEqual(self.mfas_generator.get_next_value().as_int, 0)
 
         for i in range(1, 256):
-            self.assertEqual(self.mfas_generator.get_next_value(), i)
+            self.assertEqual(self.mfas_generator.get_next_value().as_int, i)
 
-        self.assertEqual(self.mfas_generator.get_next_value(), 0)
+        self.assertEqual(self.mfas_generator.get_next_value().as_int, 0)
