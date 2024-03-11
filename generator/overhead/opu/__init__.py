5cb16c4907d1a8f73c27410998029b7b553f3ffc
ammaralmorsi <ammaralmorsi@gmail.com>
2024-03-12 00:28:32 +0200

    feat: added pjo and njo to __init__.py

--- generator/overhead/opu/__init__.py
+++ generator/overhead/opu/__init__.py
@@ -1,3 +1,5 @@
 from .jc import JCOverheadGenerator
+from .pjo import PJOOverheadGenerator
+from .njo import NJOOverheadGenerator
 from .psi import NullPSIOverheadGenerator
 from .psi import PRBSPSIOverheadGenerator
