2a52da45b2ca1627f7e16d035fee7d40587f6725
ammaralmorsi <ammaralmorsi@gmail.com>
2024-03-11 18:02:02 +0200

    refactor: added return types

--- generator/overhead/odu/aps_pcc.py
+++ generator/overhead/odu/aps_pcc.py
@@ -2,5 +2,5 @@ from generator.utils import OverheadValue
 
 
 class APS_PCCOverheadGenerator:
-    def get_next_value(self):
+    def get_next_value(self) -> OverheadValue:
         return OverheadValue(binary_string=''.zfill(4*8))
--- generator/overhead/opu/psi/null.py
+++ generator/overhead/opu/psi/null.py
@@ -2,5 +2,5 @@ from generator.utils import OverheadValue
 
 
 class NullPSIOverheadGenerator:
-    def get_next_value(self):
+    def get_next_value(self) -> OverheadValue:
         return OverheadValue(binary_string=f"{int('FD', 16):08b}")
