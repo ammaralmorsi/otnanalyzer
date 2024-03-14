3049c310aa4a98d0771b3fb30a74c952efd74f49
ahmedibrahim132002@gmail.com <ahmedibrahim132002@gmail.com>
2024-03-14 19:15:05 +0200

    feat: added the otu,fa overheads in the new structure

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 
-from src.backend.FrameBuilder.Parser_APIs import parser_API
-from Configuration.OTN_Fields_Config import OTN_OH
 
 
-filepath = "C:/Users/ashar/OneDrive/Desktop/testinggg/otnanalyzer/InputTests/input.txt"
 
-test = parser_API(filepath, "OTN")
 
-print(test.get_odu_overhead())
+
 
 
 
