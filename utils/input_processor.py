828a668cd049a80203fd9a2aef0b859a0cd1228f
ahmedibrahim132002@gmail.com <ahmedibrahim132002@gmail.com>
2024-03-23 06:12:35 +0200

    refactor: refactored input processor

--- utils/input_processor.py
+++ utils/input_processor.py
@@ -1,69 +1,30 @@
+from config.frame import OtnFrameConfig
 
 
 class InputProcessor:
+    def __init__(self, filepath: str):
+        self.file_path: str = filepath
+        self.frames: list[str] = self.read_frames()
+        self.formatted_frames: list[list[list[str]]] = self.format_frames()
 
-    def __init__(self, filepath):
-        self.file_path = filepath
-        self.frames = self.frames_file_reader()
-        self.formatted_frames = self.get_all_formatted_frames(self.frames)
+    def read_frames(self) -> list[str]:
+        frames_list = []
+        with open(self.file_path, 'r') as file:
+            for frame in file:
+                frames_list.append(frame.strip())
+        return frames_list
 
-    def frames_file_reader(self):
+    def format_frames(self) -> list[list[list[str]]]:
+        return [self.format_frame(row_frame) for row_frame in self.frames]
 
-        try:
-            frames_list = []
-            with open(self.file_path, 'r') as file:
-                for frame in file:
-                    frames_list.append(frame.strip())
-            return frames_list
-        except FileNotFoundError:
-            print(f"File '{self.file_path}' not found.")
-            return None
+    def format_frame(self, row_frame: str) -> list[list[str]]:
+        decimal_values: list[str] = row_frame.split()
+        hex_values: list[str] = self.hexlify(decimal_values)
+        return self.to_2d(hex_values)
 
-    def convert_data_to_hex(self, otn_frame_row):
+    def hexlify(self, decimal_values: list[str]) -> list[str]:
+        return [hex(int(value))[2:].zfill(2) for value in decimal_values]
 
-        """
-            Convert decimal numbers in a single-line frame to hexadecimal.
-
-            Args:
-                OTN_Frame_Row (str): A single-line frame represented as decimal numbers separated by spaces.
-
-            Returns:
-                str: The frame with decimal numbers converted to hexadecimal, separated by spaces.
-
-        """
-
-        decimal_values = otn_frame_row.split()
-        hex_values = [hex(int(value))[2:].zfill(2) for value in decimal_values]
-        single_line_hex = ' '.join(hex_values)
-        return single_line_hex
-
-
-    def format_to_otn_frame(self , HEX_OTN_Frame):
-
-        values = HEX_OTN_Frame.split()
-        rows = [values[i:i + 3824] for i in range(0, len(values), 3824)]    # '3824' hardcoded , resolve after merging branches
-        return rows
-
-    def get_all_formatted_frames(self , frames_list):
-        """
-            Format each line in the file in the needed format which is the data to be in hex ,
-            and every frame to be a list of lists [][]
-
-            Args:
-                frames_list (list) : A list of single frames
-
-            Returns:
-                 list of 2d otn frames , each 2d otn frame represent a single line in the input
-        """
-        formatted_frames = []
-        for single_frame in frames_list:
-            frame_in_hex = self.convert_data_to_hex(single_frame)
-            frame_format = self.format_to_otn_frame(frame_in_hex)
-            formatted_frames.append(frame_format)
-        return formatted_frames
-
-
-
-
-
-
+    def to_2d(self , hex_values: list[str]):
+        ncols: int = OtnFrameConfig.DEFAULT_OTU_FRAME.value.dimension.ncols
+        return [hex_values[i:i + ncols] for i in range(0, len(hex_values), ncols)]
